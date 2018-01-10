83b06174bfdb02a3cf0e1b04980fc0e53f2d2dac
Julians Iturra <ian.zephir@gmail.com>
2018-01-10 02:18:17 -0300

    improvements to FileController

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,33 +1,63 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Product;
-class FileController extends Controller {
-    public function importExportExcelORCSV(){
+
+use Excel;
+
+class FileController extends Controller
+{
+    /**
+    *   PRODUCT IMPORT VIEW
+    *
+    */
+    public function importExportExcelORCSV()
+    {
         return view('file_import_export');
     }
-    public function importFileIntoDB(Request $request){
-        if($request->hasFile('sample_file')){
+
+    /**
+    *   PRODUCT IMPORT ACTION
+    *
+    */
+    public function importFileIntoDB(Request $request)
+    {
+        if ($request->hasFile('sample_file')) {
             $path = $request->file('sample_file')->getRealPath();
-            $data = \Excel::load($path)->get();
-            if($data->count()){
-                foreach ($data as $key => $value) {
-                    $arr[] = ['name' => $value->name, 'details' => $value->details];
+            $records = Excel::load($path)->get();
+
+            if ($records->count() > 0) {
+
+                foreach ($records as $record) {
+                    if (!empty($record['name'] && !empty($record['details']))) {
+                        Product::create([
+                            'name' => $record['name'],
+                            'details' => $record['details']
+                        ]);
+                    }
                 }
-                if(!empty($arr)){
-                    \DB::table('products')->insert($arr);
-                    dd('Insert Record successfully.');
-                }
+
+                dd('Insert Record successfully.');
+
+            } else {
+                dd("The File not have records.");
             }
         }
         dd('Request data does not have any files to import.');
     }
-    public function downloadExcelFile($type){
-        $products = Product::get()->toArray();
-        return \Excel::create('expertphp_demo', function($excel) use ($products) {
-            $excel->sheet('sheet name', function($sheet) use ($products)
-            {
+
+    /**
+    *   PRODUCT EXPORT ACTION
+    *
+    */
+    public function downloadExcelFile($type)
+    {
+        $products = Product::all()->toArray();
+
+        return Excel::create('expertphp_demo', function($excel) use ($products) {
+            $excel->sheet('sheet name', function($sheet) use ($products) {
                 $sheet->fromArray($products);
             });
         })->download($type);
